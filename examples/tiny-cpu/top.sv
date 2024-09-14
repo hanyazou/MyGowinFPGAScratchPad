b327b81e0da98b9a039b31c6138a42f7f630c0e7
hanyazou <hanyazou@gmail.com>
2024-09-14 18:47:35 +0900

    examples/tiny-cpu: Output 'hello, world!' to UART TX

--- examples/tiny-cpu/top.sv
+++ examples/tiny-cpu/top.sv
@@ -379,24 +379,46 @@ module memory(
       mem['h0001] = I_LD_IH(0, 'h00);  // LD r0.h, 00h
       mem['h0002] = I_LD_IL(1, 'h01);  // LD r1.l, 01h
       mem['h0003] = I_LD_IH(1, 'h00);  // LD r1.h, 00h
-      mem['h0004] = I_LD_IL(2, 'h10);  // LD r2.l, 10h
+      mem['h0004] = I_LD_IL(2, 'h10);  // LD r2.l, 10h  LOOP0
       mem['h0005] = I_LD_IH(2, 'h00);  // LD r2.h, 00h
-      mem['h0006] = I_LD_IL(3, 'h41);  // LD r3.l, 41h
-      mem['h0007] = I_LD_IH(3, 'h00);  // LD r3.h, 00h
+      mem['h0006] = I_LD_IL(3, 'h00);  // LD r3.l, 00h  work area
+      mem['h0007] = I_LD_IH(3, 'h20);  // LD r3.h, 20h
 
+      // LOOP0
       mem['h0008] = I_SUB(0, 0, 1);    // SUB r0, r0, r1
-      //mem['h0009] = I_ST(0, 3);      // ST r0, (r3)
-      mem['h0009] = I_NOP();
+      mem['h0009] = I_ST(0, 3);        // ST r0, (r3)
       mem['h000a] = I_STB(0, 3);       // ST r0.l, (r3)
       mem['h000b] = I_LD_IL(0, 'hff);  // LD r0.l, FFh
-      //mem['h000c] = I_LD_M(0, 3);    // LD r0, (r3)
-      mem['h000c] = I_NOP();
+      mem['h000c] = I_LD_M(0, 3);      // LD r0, (r3)
       mem['h000d] = I_LD_MB(0, 3);     // LD r0.l, (r3)
       mem['h000e] = I_JPNZ(2);         // JPNZ (r2)
       mem['h000f] = I_NOP();           // NOP
-      mem['h0010] = I_HALT();          // HALT
 
-      mem['h0020] = 'h0000;            // work area
+      mem['h0010] = I_LD_IL(2, 'h2c);  // LD r2.l, 2ch  LOOP1
+      mem['h0011] = I_LD_IH(2, 'h00);  // LD r2.h, 00h
+      mem['h0012] = I_LD_IL(3, 'h20);  // LD r3.l, 20h  message
+      mem['h0013] = I_LD_IH(3, 'h20);  // LD r3.h, 20h
+      mem['h0014] = I_LD_IL(4, 'h00);  // LD r4.l, 00h  UART TX
+      mem['h0015] = I_LD_IH(4, 'h00);  // LD r4.h, 00h
+
+      // LOOP1
+      mem['h0016] = I_LD_MB(0, 3);     // LD r0.l, (r3)
+      mem['h0017] = I_OUTB(0, 4);      // OUTB r0, (r4)
+      mem['h0018] = I_ADD(3, 3, 1);    // ADD r3, r3, r1  r3 = r3 + 1
+      mem['h0019] = I_ADD(0, 0, 0);    // ADD r0, r0, r0  r0 == 0 ?
+      mem['h001a] = I_JPNZ(2);         // JPNZ (r2)
+
+      mem['h001b] = I_HALT();          // HALT
+
+      mem['h1000] = 'h0000;            // work area
+      mem['h1010] = { "e", "H" };      // message
+      mem['h1011] = { "l", "l" };
+      mem['h1012] = { ",", "o" };
+      mem['h1013] = { "w", " " };
+      mem['h1014] = { "r", "o" };
+      mem['h1015] = { "d", "l" };
+      mem['h1016] = { 8'h0d, "!" };
+      mem['h1017] = { 8'h00, 8'h0a };
    end
 
    always @(posedge clk) begin
