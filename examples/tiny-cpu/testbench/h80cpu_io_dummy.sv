746bfbc0d5b74c1f4f81a87b587b3707e11b2092
hanyazou <hanyazou@gmail.com>
2024-09-22 14:14:29 +0900

    examples/tiny-cpu: Add console output of dummy I/O

--- examples/tiny-cpu/testbench/h80cpu_io_dummy.sv
+++ examples/tiny-cpu/testbench/h80cpu_io_dummy.sv
@@ -11,4 +11,33 @@ module h80cpu_io(
    output wire uart_txp
    );
 
+   reg prev_clk;
+   
+   initial begin
+      done <= 0;
+   end
+
+   always @(posedge sysclk or negedge sysclk) begin
+      rd_data <= 0;
+      prev_clk <= clk;
+      if (reset) begin
+         done <= 0;
+      end else begin
+      // posedge clk
+      if (~prev_clk && clk)  begin
+         if (run != done) begin
+            case (addr)
+            'h0000: begin
+               if (cmd == bus_cmd_write_b) begin
+                  $write("%c", wr_data[7:0]);
+               end
+            end
+            endcase
+            done <= ~done;
+         end
+      end
+      end // else: !if(reset)
+
+   end // always @ (posedge sysclk)
+
 endmodule
