820394af76204cd31cf0be88e3c1d6d5d5e2f028
hanyazou <hanyazou@gmail.com>
2024-10-05 14:19:59 +0900

    examples/tiny-cpu: Add h80cpu_mem.sv and rom_*.svh

---
+++ examples/tiny-cpu/h80cpu_mem.sv
@@ -0,0 +1,53 @@
+module h80cpu_mem(
+   input wire clk,
+   input wire reset,
+   input wire ce_n,
+   input wire bus_addr_t addr,
+   input wire bus_cmd_t cmd,
+   inout wire bus_data_t data_,
+   output wire wait_n
+   );
+
+   reg [15:0] mem[1024*32];  // 32K words
+   reg [15:0] rd_data;
+   int state = 0;
+
+   assign wait_n = (!ce_n && state != 0) ? 1'b0 : 1'b1;
+   assign data_ = (!ce_n && cmd[0]) ? rd_data : {16{1'bz}};
+
+   initial begin
+      // `include "rom_hello.svh"
+      `include "rom_mandelbrot.svh"
+   end
+
+   always @(posedge clk) begin
+      if (reset) begin
+         state <= 0;
+      end else begin
+         case (state)
+         0: begin
+            if (!ce_n) begin
+               case (cmd)
+               bus_cmd_read_w:
+                  rd_data <= mem[addr[15:1]];
+               bus_cmd_write_w:
+                  mem[addr[15:1]] <= data_;
+               bus_cmd_read_b:
+                 if (addr[0])
+                   rd_data <= { 8'h00, mem[addr[15:1]][15:8] };
+                 else
+                   rd_data <= { 8'h00, mem[addr[15:1]][7:0] };
+               bus_cmd_write_b:
+                 if (addr[0])
+                   mem[addr[15:1]] <= { data_[7:0], mem[addr[15:1]][7:0] };
+                 else
+                   mem[addr[15:1]] <= { mem[addr[15:1]][15:8], data_[7:0] };
+               endcase
+               state <= 0;  // there is only one state, no transition
+            end
+         end
+         endcase
+      end // else: !if(reset)
+   end // always @ (posedge clk)
+
+endmodule
